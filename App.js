622f57769896e1db79f714424f19f74d7e404b61
Florian VERVISCH <floki93@yahoo.com>
2020-07-21 16:09:20 +0200

    msg rajoute

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useState}from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
@@ -6,35 +6,83 @@ export default function App() {
   let [points, setPoints] = useState(0)
   let [hp, setHp] = useState(3)
   let [jokers, setJokers] = useState(2)
-  const questions = require("./questions.json");
-  let [round, setRound] = useState(0);
+  const questions = require("./questions.json")
+  let [round, setRound] = useState(0)
+  let [hideButtonNext, setHideButtonNext] = useState(true)
+  let [complementary, setComplementary] = useState("")
+  let [msgRound, setMsgRound] = useState("")
 
   const blueColor = "#32a9de"
   const greenColor = "#32de44"
   const redColor = "#de3232"
-  
+
   const checkResponse = (question, response) => {
     if (question.response === response) {
       setPoints(++points)
+      setMsgRound("Gagné !")
+      setHideButtonNext(false);
     } else {
       setHp(--hp)
+      if (hp > 0) {
+        setHideButtonNext(false);
+      }
+      setMsgRound("Perdu !")
+      setComplementary(questions[round].complementary)
     }
-    setRound(++round)
   }
 
-  return (<View style={styles.container}>
-    <Text>Points : {points}</Text>
-    <Text>Vie(s) : {hp}</Text>
-    <Text>{questions[round].question}</Text>
+  const next = () => {
+    setComplementary("")
+    setRound(++round)
+    setHideButtonNext(true)
+    setMsgRound("")
+  }
+
+  function Quizz(props) {
+    if (hp <= 0) {
+      return (<View><Text>Tu as perdu bg</Text></View>)
+    }
+
+    if (round > (questions.length-1)) {
+      return (<View><Text>Tu as gagné bg</Text></View>)
+    }
+
+    return (<View style={styles.container}>
+      <Text>Points : {points}</Text>
+      <Text>Vie(s) : {hp}</Text>
+      <Text>{msgRound}</Text>
+      <Text>{questions[round].question}</Text>
       <Button
-          title="True"
-          color={greenColor}
-          onPress={() => checkResponse(questions[round], true)} />
+        title="True"
+        color={greenColor}
+        onPress={() => checkResponse(questions[round], true)} />
       <Button
-          title="False"
-          color={redColor}
-          onPress={() => checkResponse(questions[round], false)} />
-    <Text>{round}</Text>
+        title="False"
+        color={redColor}
+        onPress={() => checkResponse(questions[round], false)} />
+      <Text>Round : {round}</Text>
+      <Text>{complementary}</Text>
+    </View>)
+  }
+
+  function NextButton(props) {
+    if (hideButtonNext == false) {
+      return (<View>
+        <Button 
+          title="Next" 
+          color={blueColor}
+          onPress={() => next()}
+          />
+        </View>)
+    } else {
+      return (<View></View>)
+    }
+  }
+
+  return (
+  <View style={styles.container}>
+    <Quizz/>
+    <NextButton/>
   </View>);
 }
 
